4be08d593bb40b52e25875a6987b1c7b1aa44e5a
joaovitorpiccolo <joaovitorpiccolo015@gmail.com>
2025-09-05 22:38:02 -0300

    finishing project

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,52 +1,31 @@
 import "../css/global.css"
 import "../css/resultado.css"
+import TabelaImc from "./TabelaImc"
 
-const TabelaImc=(resultado)=>{
-    if(resultado < 18){
-        return (<>
-            <td>Abaixo do Peso</td>
-            <td>Abaixo de 18</td>
-        </>)
-    }
-    else if (resultado > 18 && resultado < 25){
-        return (
-            <>
-            <td>Peso Normal</td>
-            <td>18 - 24.9</td>
-            </>
-        )
-    }else if (resultado > 24 && resultado < 30){
-        return (
-            <>
-            <td>Sobre Peso</td>
-            <td>25 - 29.9</td>
-            </>
-        )}else if (resultado > 29 && resultado < 35){
-        return (
-            <>
-            <td>Obesidade</td>
-            <td>20 - 34.9</td>
-            </>
-        )}else{
-            return (
-            <>
-            <td>Obesidade Morbida</td>
-            <td>maior ou igual a 40</td>
-            </>)
-        }
-};
-
-function Resultado(resultado){
-    return(
-        <>
+//destruct - Desestruturação - acessa os dados de outro componente
+function Resultado({resultado}){
+    return (
         <div>
             <div className="tabela">
                 <h2>
-                    seu IMC é de: <span className="imcSpan">{resultado}</span>
+                    Seu IMC é de: <span class name="imcSpan">{resultado}</span>
                 </h2>
+
             </div>
-            <table></table>
+
+            <table className='tabela'>
+                <thead className='tabelaHeader'>
+                    <tr>
+                        <th>Classificação</th>
+                        <th>IMC</th>
+                    </tr>
+                </thead>
+                <tbody className="tabelaBody">
+                    <tr>{TabelaImc(resultado)}</tr>
+                </tbody>
+
+            </table>
         </div>
-        </>
     )
 }
+export default Resultado
