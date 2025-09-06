a5669124f43190bf4b0e15f5dd335715418cc3c4
joaovitorpiccolo <joaovitorpiccolo015@gmail.com>
2025-09-05 22:57:57 -0300

    starting js

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,6 @@ import './css/estilo.css'
 function App() {
 
   // HOOK- useState - Manipula o estado da variável
-  const[altura,setAltura]=useState();
-  const [peso,setPeso]=useState();
-  const[resultado,setResultado]=useState();
-  const [mostrarResultado,setMostrarResultado]=useState(false);
 
   //CRIANDO A FUNÇAO CALCULAR IMC
   const calcularImc=(e)=>{
